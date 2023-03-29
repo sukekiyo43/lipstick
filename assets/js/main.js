186673aca884273be4a4e901ca033d9ab7c1f424
sukekiyo43 <teihen13@infoseek.jp>
2023-03-30 07:11:03 +0900

    モーダルウィンドウ不具合修正

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,27 @@
 
 /// モーダルウィンドウ制御 ///
 
+// スクロールバーの幅の取得
+let body = document.body;
+let clientWidth = body.clientWidth;
+let noScrollBarWidth = clientWidth;
+let diff = 0;
+
 // モーダルウィンドウ開
 modal_open.addEventListener('click', function () {
     modal_open_cross.classList.toggle('open');
     modal_store_open_cross.classList.toggle('open');
+    return_circle.classList.toggle('open');
     window.setTimeout(function () {
         modal_store.classList.toggle('open');
+        clientWidth = body.clientWidth;
+        body.classList.toggle('open');
+        // スクロールバーの長さ計算
+        noScrollBarWidth = body.clientWidth;
+        diff = noScrollBarWidth - clientWidth;
+        if (diff > 0) {
+            body.style['padding-right'] = diff + 'px';
+        }
     }, 1000);
 });
 
@@ -15,10 +30,12 @@ modal_store_open_cross.addEventListener('click', function () {
     modal_store_open_cross.classList.toggle('open');
     modal_open_cross.classList.toggle('open');
     window.setTimeout(function () {
+        body.style['padding-right'] = '0px';
+        body.classList.toggle('open');
         modal_store.classList.toggle('open');
+        return_circle.classList.toggle('open');
     }, 1000);
 });
-
 
 /// ドロップダウンメニュー制御 ///
 
